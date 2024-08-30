4ec79c1f614bdc7b4529a2381c47683e65471b1d
Imran Naeem <imran.n@gltexpress.com>
2024-08-30 18:59:58 +0500

    improvements suggested for tenant-invoice-settings added

--- app/controller/api.js
+++ app/controller/api.js
@@ -185,19 +185,16 @@ router.post(
   }
 );
 
-router.post(
-  ['/platform/zid/validate/and/integrate'],
-  async (req, res, next) => {
-    try {
-      return await requestHandler(req, res, next, requestForwarder);
-    } catch (error) {
-      next(error);
-    }
+router.post('/platform/zid/validate/and/integrate', async (req, res, next) => {
+  try {
+    return await requestHandler(req, res, next, requestForwarder);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 //MAN
-router.post(['/platform/zid/auto/dispatching'], async (req, res, next) => {
+router.post('/platform/zid/auto/dispatching', async (req, res, next) => {
   try {
     return await requestHandler(req, res, next, requestForwarder);
   } catch (error) {

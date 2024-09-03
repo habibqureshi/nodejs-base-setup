03569cb756ff93e17b020f6d4371dd4249fb6021
Imran Naeem <imran.n@gltexpress.com>
2024-09-03 13:12:40 +0500

    testing intra tenant

--- app/utils/url-redirect.js
+++ app/utils/url-redirect.js
@@ -1,5 +1,5 @@
 const managementBackEnd = process.env.MANAGEMENT_BACKEND;
-const lastMileBackend = 'http://localhost:10000';
+const lastMileBackend = process.env.LATS_MILE_BACKEND;
 const fulfilmentBackend = process.env.FULFILLMENT_BACKEND;
 
 let urls = {
